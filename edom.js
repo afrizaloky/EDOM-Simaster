e68bdcc482c76b85c4de13752682c184a3615204
afrizaloky <afrizaloky@outlook.com>
2020-01-29 21:54:01 +0700

    Update edom.js

--- edom.js
+++ edom.js
@@ -1,9 +1,15 @@
+// 1 = Sangat Baik
+// 2 = Baik
+// 3 = Cukup
+// 4 = Kurang
+var nilai = "2"; // Edit penilaian dosen
+
 for (i = 0; i < 13; i++) {
   var elements = document.getElementsByName(
     "jawabanInstrumenPilihan[" + i + "]"
   );
   for (j = 0; j < elements.length; j++) {
-    if (elements[j].value == "2") {
+    if (elements[j].value == nilai) {
       elements[j].checked = true;
     }
   }
